70b4c72a2f20158b7a93f4477890e44ceee79ccf
exaltedsol04 <exaltedsol04@gmail.com>
2025-10-29 11:11:10 +0530

    comment

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,11 +4,25 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
+use App\Models\Grievance;
 
 class AdminController extends Controller
 {
     public function dashboard()
     {
-        return view('admin.dashboard');
+		$data = [];
+		
+		$tot_grievance = Grievance::count();
+		$pending_grievance = Grievance::whereIn('status', [1,2])->count();
+		$solved_grievance = Grievance::where('status', 3)->count();
+		$alert_grievance = Grievance::whereIn('status', [1,2])->where('created_at', '<=', Carbon::now()->subDays(3))->count();
+		
+		$data['tot_grievance'] = $tot_grievance;
+		$data['pending_grievance'] = $pending_grievance;
+		$data['solved_grievance'] = $solved_grievance;
+		$data['alert_grievance'] = $alert_grievance;
+		
+        return view('admin.dashboard', $data);
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,8 +5,57 @@
 	<div class="content container-fluid">
 		<div class="page-header">
 			<div class="content-page-header">
-				<h5>Accordions</h5>
+				<h5>Dashboard</h5>
 			</div>	
+		</div>
+		<!-- /Page Header -->
+		
+				
+		<div class="row">
+			<div class="col-md-6 col-sm-6 col-lg-6 col-xl-3">
+				<div class="card dash-widget">
+					<div class="card-body">
+						<span class="dash-widget-icon"><i class="fa-solid fa-cubes"></i></span>
+						<div class="dash-widget-info">
+							<h3>{{ $tot_grievance ?? ''}}</h3>
+							<span>Total grievance</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6 col-sm-6 col-lg-6 col-xl-3">
+				<div class="card dash-widget">
+					<div class="card-body">
+						<span class="dash-widget-icon"><i class="fa-solid fa-dollar-sign"></i></span>
+						<div class="dash-widget-info">
+							<h3>{{ $pending_grievance ?? ''}}</h3>
+							<span>Pending grievance</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6 col-sm-6 col-lg-6 col-xl-3">
+				<div class="card dash-widget">
+					<div class="card-body">
+						<span class="dash-widget-icon"><i class="fa-regular fa-gem"></i></span>
+						<div class="dash-widget-info">
+							<h3>{{ $solved_grievance ?? ''}}</h3>
+							<span>Solved grievance</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-6 col-sm-6 col-lg-6 col-xl-3">
+				<div class="card dash-widget">
+					<div class="card-body">
+						<span class="dash-widget-icon"><i class="fa-solid fa-user"></i></span>
+						<div class="dash-widget-info">
+							<h3>{{ $alert_grievance ?? '' }}</h3>
+							<span>Alert grievance</span>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
