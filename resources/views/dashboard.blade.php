02868a74ecb0e97769b74e5e658917002425cce0
Exalted 06 <exaltedsol06@gmail.com>
2025-10-29 19:43:34 +0530

    add

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -56,6 +56,7 @@ use Carbon\Carbon;
 															<th>{{ __('received_date') }}</th>
 															<th>{{ __('grievance_description') }}</th>
 															<th>{{ __('status') }}</th>
+															<th class="text-center" style="width:112px">{{ __('action') }}</th>
 														</tr>
 													</thead>
 													<tbody>
@@ -65,6 +66,17 @@ use Carbon\Carbon;
 															<td>{{ Carbon::parse($grievance->created_at)->format('d/m/y') }}</td>
 															<td>{{ \Illuminate\Support\Str::words($grievance->issue_description, 15, '...') }}</td>
 															<td class="{{ $grievance->status==1 ? 'text-danger' : ($grievance->status==2 ? 'text-info' : 'text-success') }}">{{ $grievance->status==1 ? 'Pending' : ($grievance->status==2 ? 'Resubmit' : 'Solved') }}</td>
+															<td>
+																@if(auth()->user()->user_type == 1)
+																<ul class="custom-small-box">
+																	<li><a href="javascript:void(0)" data-url="{{ url('delete-grievance') }}" data-id="{{ $grievance->id }}" class="delete-grievance"><i class="fa-solid fa-trash text-danger"></i></a></li>
+																	<li><a href="{{ url('edit-grievance', ['id'=> $grievance->id]) }}"><i class="fa-solid fa-pen text-success"></i></a></li>
+																</ul>
+															@endif
+																<ul class="custom-small-box">
+																	<li><a href="{{ route('view-grievance', ['id'=> $grievance->id]) }}"><i class="fa-solid fa-eye text-warning"></i></a></li>
+																</ul>
+															</td>
 														</tr>
 													@endforeach
 													</tbody>
@@ -82,6 +94,7 @@ use Carbon\Carbon;
     </div>
 @endsection 
 @section('scripts')
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script type="text/javascript">
 	if($('.datatable').length > 0) {
 		$('.datatable').DataTable({
@@ -96,13 +109,55 @@ use Carbon\Carbon;
 		});
 		
 	}
-</script>
-<script>
-$(document).ready(function() {
-	$(document).on('click', '.viewgrievance', function(){
-		window.location.href = $(this).data('href');
+	$(document).ready(function() {
+		$(document).on('click', '.viewgrievance', function(){
+			window.location.href = $(this).data('href');
+		});
+		$(document).on('click', '.delete-grievance', function(){
+			Swal.fire({
+				title: 'Are you sure?',
+				text: "You won't be able to revert this!",
+				icon: 'warning',
+				showCancelButton: true,
+				confirmButtonColor: '#3085d6',
+				cancelButtonColor: '#d33',
+				confirmButtonText: 'Yes, delete it!',
+				cancelButtonText: 'Cancel'
+			}).then((result) => {
+				if (result.isConfirmed) {
+					let URL = $(this).data('url');
+					let id = $(this).data('id');
+					//let moreload = $('#moreload').val();
+					$.ajax({
+						url: URL,
+						type: "POST",
+						data: {
+							id : id,
+							//moreload:moreload,
+							_token: "{{ csrf_token() }}"
+						},
+						dataType: 'json',
+						success: function(response) {
+							//alert(response.html);
+							//$('#moreload').val(response.loadmore);
+							//$('#show-list-data').append(response.html);
+							
+							 Swal.fire({
+								title: 'Deleted!',
+								text: 'Your record has been deleted.',
+								icon: 'success',
+								timer: 1500,
+								showConfirmButton: false
+							});
+						},
+						error: function(xhr) {
+							console.error(xhr.responseText);
+						}
+					});
+				}
+			});
+		});
 	});
-});
 </script>
 @endsection
 
