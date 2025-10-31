ab42c913734cce8292bef3f19ba72320564c578e
Exalted02 <exaltedsol02@gmail.com>
2025-10-31 10:29:02 +0530

    up

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\RegisteredUserController;
+use App\Http\Controllers\Api\GrievanceController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +19,20 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/google/callback', [RegisteredUserController::class, 'googleLogin']);
+Route::post('login', [RegisteredUserController::class, 'login'])->name('login');
+Route::post('store-citizen', [RegisteredUserController::class, 'store_citizen'])->name('store-citizen');
+// Route::post('store-retailer', [RegisteredUserController::class, 'store_retailer'])->name('store-retailer');
+Route::post('forgot-password', [RegisteredUserController::class, 'forgotPassword'])->name('forgot-password');
+Route::post('forgot-password-verify-otp', [RegisteredUserController::class, 'forget_password_verify_otp'])->name('forgot-password-verify-otp');
+Route::post('reset-password', [RegisteredUserController::class, 'resetpassword'])->name('reset-password');
+// Route::get('/retailer_list',[RegisteredUserController::class, 'all_retailer_list']);
+
+Route::post('register-verify-otp', [RegisteredUserController::class, 'register_verify_otp'])->name('register-verify-otp');
+
+Route::post('/edit-citizen-profile',[RegisteredUserController::class, 'edit_citizen_profile']);
+Route::post('/edit-retailer-profile',[RegisteredUserController::class, 'edit_retailer_profile']);
+Route::post('/change-user-password',[RegisteredUserController::class, 'change_password']);
+
+Route::get('/grievance-lists',[GrievanceController::class, 'grievance_lists']);
