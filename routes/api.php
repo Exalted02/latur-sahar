ae80eb80742c2e929fdeffdd82c9be3e088bbed8
exaltedsol04 <exaltedsol04@gmail.com>
2025-11-04 16:23:17 +0530

    comment

--- routes/api.php
+++ routes/api.php
@@ -44,3 +44,4 @@ Route::post('/submit-rating',[GrievanceController::class, 'submit_rating']);
 
 Route::post('/submit-grievance',[GrievanceController::class, 'submit_grievance']);
 Route::get('/department-lists',[GrievanceController::class, 'department_lists']);
+Route::get('/ward-prabhag-lists',[GrievanceController::class, 'ward_prabhag_lists']);
